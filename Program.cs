e155db478ce27998de9e2fa0e28915f5478956bf
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2019-03-04 19:41:08 +1300

    started dynamic programming

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        public static long MoneyChange(long coins)
+        {
+            var MemoArray = new long[coins +1];
+            MemoArray[0] = 0;
+            for(var i =1; i <= coins; i++)
+            {
+                var Using1Coin = MemoArray[i - 1];
+                if (i>=3)
+                {
+                    Using1Coin = Math.Min(Using1Coin, MemoArray[i - 3]);
+                }
+                if (i >= 4)
+                {
+                    Using1Coin = Math.Min(Using1Coin, MemoArray[i - 4]);
+                }
+
+                MemoArray[i] = Using1Coin  +1;
+            }
+            return MemoArray[coins];
+        }
+
+        public static long[] Calculations(long numberToGet)
+        {
+
+
+            var MemoArray = new long[numberToGet + 1];
+            MemoArray[0] = 1;
+            var MemoPointers = new long[numberToGet + 1];
+
+            for(var i=2; i<=numberToGet; i++)
+            {
+                MemoArray[i] = MemoArray[i - 1] +1;
+                MemoPointers[i] = i - 1;
+                if (i % 2 == 0 && MemoArray[i/2] < MemoArray[i])
+                {
+
+                    MemoArray[i] = MemoArray[i / 2] + 1;
+                    MemoPointers[i] = i / 2;
+
+                }
+                if(i % 3 == 0 && MemoArray[i/3] < MemoArray[i])
+                {
+                    MemoArray[i] = MemoArray[i / 3] + 1;
+                    MemoPointers[i] = i / 3;
+
+                }
+            }
+            var OptimalSeq = new List<long>();
+
+            while(numberToGet > 0)
+            {
+                OptimalSeq.Add(numberToGet);
+                numberToGet = MemoPointers[numberToGet];
+            }
+
+            return OptimalSeq.AsEnumerable().Reverse().ToArray();
+        }
+
+        public static long[] OptimalSequence(long numberToGet)
+        {
+            var OptimalSeq = new List<long>();
+            while (numberToGet >= 1)
+            {
+                OptimalSeq.Add(numberToGet);
+                if(numberToGet % 3 == 0)
+                {
+                    numberToGet /= 3;
+                }
+                else if(numberToGet %2 == 0)
+                {
+                    numberToGet /= 2;
+                }
+                else
+                {
+                    numberToGet -= 1;
+                }
+            }
+            return OptimalSeq.AsEnumerable().Reverse().ToArray();
+        }
+    
+        static void Main(string[] args)
+        {
+            var coins = Convert.ToInt64(Console.ReadLine());
+
+            var res = Calculations(coins);
+            Console.WriteLine(res.Length-1);
+            Console.WriteLine(string.Join(" ", res));
+
+
+        }
+    }
+}
---
+++ quickSort3.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class quickSort3
+    {
+
+        public class Indexes
+        {
+            public int Lindex { get; set; }
+            public int Rindex { get; set; }
+        }
+
+        public static Indexes Partiton(long[] inputArray, int LIndex, int Rindex)
+        {
+
+            var Random = new Random();
+            var Middle = Random.Next(LIndex, Rindex);
+
+
+            long temp = 0;
+            long middleElement = inputArray[Middle];
+            //move the middle to the start of the Array
+            inputArray[Middle] = inputArray[LIndex];
+            inputArray[LIndex] = middleElement;
+
+
+            //current index of the end of each section
+            var currentL = LIndex;
+            var currentM = LIndex + 1;
+            var currentR = LIndex + 1;
+            //sort by the middle element, starting from the element to the right of element(s) to go in the middle
+            for (var i = LIndex + 1; i <= Rindex; i++)
+            {
+                if (inputArray[i] > middleElement)
+                {
+                    currentR++;
+                }
+                else if (inputArray[i] == middleElement && i <= Rindex)
+                {
+                    inputArray[i] = inputArray[currentM];
+                    inputArray[currentM] = middleElement;
+                    currentM++;
+                    currentR++;
+
+
+                }
+                else
+                {
+                    //temp = inputArray[currentL]; //first M element;
+
+                    inputArray[currentL] = inputArray[i];
+
+                    if (currentR > currentM)
+                    {
+                        inputArray[i] = inputArray[currentM];
+
+                    }
+                    else
+                    {
+                        inputArray[i] = middleElement;
+                    }
+
+                    inputArray[currentM] = middleElement;
+                    currentR++;
+                    currentM++;
+                    currentL++;
+
+                }
+            }
+            return new Indexes
+            {
+                Lindex = currentL,
+                Rindex = currentM
+            };
+        }
+
+
+        public static long[] QuickSort3(long[] inputArray, int LIndex, int Rindex)
+        {
+            var Indexes = Partiton(inputArray, LIndex, Rindex);
+
+
+
+
+            if ((Indexes.Lindex) > LIndex)
+            {
+                QuickSort3(inputArray, LIndex, Indexes.Lindex);
+            }
+            if (Indexes.Rindex < Rindex)
+                QuickSort3(inputArray, Indexes.Rindex, Rindex);
+
+            return inputArray;
+
+        }
+
+        public static long[] QS3(long[] inputArray)
+        {
+            if (inputArray.Length == 0)
+            {
+                return inputArray;
+            }
+            var LIndex = 0;
+            var RIndex = inputArray.Length - 1;
+            return QuickSort3(inputArray, LIndex, RIndex);
+
+
+
+        }
+
+        public static Boolean ArraysEqual(long[] mySortedArray, long[] correctlySortedArray)
+        {
+            if (mySortedArray.Length != correctlySortedArray.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < mySortedArray.Length; i++)
+            {
+                if (mySortedArray[i] != correctlySortedArray[i])
+                {
+                    var myStoredi = mySortedArray[i];
+                    var correctSortedi = correctlySortedArray[i];
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    var valuesToFind = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c)).Skip(1).ToArray();
+        //    var inputArray = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c)).ToArray();
+
+        //    var count = 0;
+        //    //while (true)
+        //    //{
+        //    //    var random = new Random();
+        //    //    var ArrLength = random.Next(0, 1000000);
+        //    //    var newArray = new long[ArrLength];
+        //    //    var newArray2 = new long[ArrLength];
+        //    //    var baseLine = new long[ArrLength];
+        //    //    for (var i=0; i< ArrLength;i++)
+        //    //    {
+        //    //        newArray[i] = random.Next(0, 100000000);
+        //    //        newArray2[i] = newArray[i];
+        //    //        baseLine[i] = newArray[i];
+        //    //    }
+        //    //    QS3(newArray);
+        //    //    newArray2 = newArray2.OrderBy(x => x).ToArray();
+        //    //    ArraysEqual(newArray, newArray2);
+        //    //    count++;
+
+        //    //}
+
+        //    var res = QS3(inputArray);
+        //    Console.WriteLine(string.Join(" ", res));
+
+
+        //}
+    }
+}
