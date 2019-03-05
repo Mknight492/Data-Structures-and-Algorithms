15af80878a1c91c0c3842003d735e3b0d86f17f8
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2019-03-05 20:38:49 +1300

    finished knapsack with discrete values

--- KnapSackDiscrete.cs
+++ KnapSackDiscrete.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace Addition
 {
-    class Program
+    class KnapSackDiscrete
     {
 
-        public static long GoldBars(long maxWeight,long[] GoldBarArray)
+        public static long GoldBars(long maxWeight, long[] GoldBarArray)
         {
             //initialise lookuptable with 
-            var LookupTable = new long[GoldBarArray.Length + 1, maxWeight +1];
+            var LookupTable = new long[GoldBarArray.Length + 1, maxWeight + 1];
 
 
-            for(long goldBar =1; goldBar <= GoldBarArray.Length; goldBar++)
+            for (long goldBar = 1; goldBar <= GoldBarArray.Length; goldBar++)
             {
                 //weight of the current gold bar
                 long currentWeight = GoldBarArray[goldBar - 1];
                 for (long weight = 1; weight <= maxWeight; weight++)
                 {
-                    
-                    if(currentWeight <= weight)
+
+                    if (currentWeight <= weight)
                     //the currentWeight might be able to be used as it wount automatically make the "knapsack too heavy"
                     {
                         //return whichever is higher the prev. calculated maxium 
@@ -35,7 +35,7 @@ namespace Addition
                     else
                     {
                         //the current weight can be used yet so return the previous calculated maximum at this weight
-                        LookupTable[goldBar, weight] = LookupTable[goldBar -1, weight];
+                        LookupTable[goldBar, weight] = LookupTable[goldBar - 1, weight];
                     }
 
                 }
@@ -43,19 +43,16 @@ namespace Addition
             return LookupTable[GoldBarArray.Length, maxWeight];
         }
 
-    
+
         static void Main(string[] args)
-        {
+        //{
 
-            var MaxWeight = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c)).ToArray()[0];
-            var GoldBarArray = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c));
+        //    var MaxWeight = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c)).ToArray()[0];
+        //    var GoldBarArray = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c));
 
-            //var word2 = Console.ReadLine();
+        //    var res = GoldBars(MaxWeight, GoldBarArray);
+        //    Console.WriteLine(res);
 
-            var res = GoldBars(MaxWeight, GoldBarArray);
-            Console.WriteLine(res);
-
-
-        }
+        //}
     }
 }
