321071f63af5437f675d40fbc56eaf03b30746a0
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2019-03-08 11:59:27 +1300

    completed a heap sort

--- Program.cs
+++ Program.cs
@@ -18,66 +18,160 @@ namespace Addition
             public List<Node> Nodes = new List<Node>();
         }
 
-
-        public static long MaxDepth(Node node)
+        public class Swap
         {
-            var counter = 0;
- 
-           
-            while(node.Nodes.Count == 1)
-            {
-                counter++;
-                node = node.Nodes[0];
-            }
-            if (node.Nodes.Count == 0)
-            {
-                return counter;
-            }
-            else
-            {
-                var lengths = new long[node.Nodes.Count];
-                for(var i =0; i < node.Nodes.Count; i++)
-                {
-                    lengths[i] = MaxDepth(node.Nodes[i]);
-                }
-                return 1 + counter + lengths.Max();
-            }
+            public int start { get; set; }
+            public int end { get; set; }
         }
 
-        public static long MaxTreeDepth(long[] nodes)
+
+        public static void CheckHeap(long[] arr, int index)
         {
-            var NodeArray = new Node[nodes.Length];
-            Node Root = new Node();
-            //initialise the tree
-            for (var i= 0; i<nodes.Length; i++)
+            if (index * 2 + 1 < arr.Length)
             {
-                NodeArray[i] = new Node();
-            }
-            for (var i = 0; i < nodes.Length; i++)
-            {
-                if(nodes[i] == -1)
+                //check left child
+                var RChild = arr[index * 2 +1];
+                
+                //is Lchild the smallest
+                if (RChild < arr[index])
                 {
-                    Root = NodeArray[i];
+                    Console.WriteLine("not sorted");
                 }
-                else
+                else if (RChild > arr[index])
                 {
-                    var parent = nodes[i]; //-1 as input not 0 indexed
-                    NodeArray[parent].Nodes.Add(NodeArray[i]);
+                    CheckHeap(arr, index * 2 +1);
                 }
             }
-            return MaxDepth(Root);
+            if (index * 2 < arr.Length)
+            {
+                //check left child
+                var LChild = arr[index * 2];
+
+                //is Lchild the smallest
+                if (LChild < arr[index])
+                {
+                    Console.WriteLine("not sorted");
+                }
+                else if (LChild > arr[index])
+                {
+                    CheckHeap(arr, index * 2);
+                }
+            }
+
         }
+
+
+
+
+        public static void DownShift(long nodeValue, int index, long[] arrpointer, List<Swap> swaps)
+        {
+            var currentPosition = index;
+            var notChecked = true;
+            //check current position has two leaf nodes
+           while (currentPosition*2 +1 < arrpointer.Length && notChecked){
+                //check left child
+                var LChild = arrpointer[currentPosition * 2];
+                var RChild = arrpointer[currentPosition * 2 +1];
+                //is Lchild the smallest
+                if (LChild <= RChild && LChild < nodeValue)
+                {
+                    arrpointer[currentPosition] = arrpointer[currentPosition * 2];
+                    arrpointer[currentPosition * 2] = nodeValue;
+                    swaps.Add(new Swap
+                    {
+                        start = currentPosition,
+                        end = currentPosition * 2
+                    });
+                    currentPosition = currentPosition * 2;
+                }
+                //is RChild the smallest
+                else if(RChild <= LChild && RChild < nodeValue)
+                {
+                    arrpointer[currentPosition] = arrpointer[currentPosition * 2 +1];
+                    arrpointer[currentPosition * 2 +1] = nodeValue;
+                    swaps.Add(new Swap
+                    {
+                        start = currentPosition,
+                        end = currentPosition * 2 +1
+                    });
+                    currentPosition = currentPosition * 2 +1;
+                }
+                if (LChild >= nodeValue && RChild >= nodeValue)
+                {
+                    notChecked = false;
+                }
+            }
+           //check if their is a single leaf node
+            if(currentPosition * 2  < arrpointer.Length   && arrpointer[currentPosition * 2] < nodeValue)
+            {
+                var temp = nodeValue;
+                arrpointer[currentPosition] = arrpointer[currentPosition * 2];
+                arrpointer[currentPosition * 2] = temp;
+                swaps.Add(new Swap
+                {
+                    start = currentPosition,
+                    end = currentPosition * 2
+                });
+                currentPosition = currentPosition * 2;
+            }
+            //check right child
+        }
+
+
+
+        public static long[] MinHeapSort(long[] arr)
+        {
+            //will use array indexing of 1
+            var n= (int)Math.Floor((arr.Length -1)/ (double)(2));
+            var swaps = new List<Swap>();
+            for(var i = n; i>=1; i--)
+            {
+                var node = arr[i];
+                DownShift(node, i, arr, swaps );
+            }
+            //DownShift(arr[1], 1, arr, swaps);
+
+            Console.WriteLine(swaps.Count());
+            foreach(var swap in swaps)
+            {
+                Console.WriteLine($"{swap.start - 1} {swap.end - 1}");
+            }
+            if (arr.Length >= 1 && (swaps.Count > (arr.Length - 1) * 4))
+            {
+                Console.WriteLine("too many swaps");
+            }
+            return arr;
+        }
+
+
 
 
         static void Main(string[] args)
         {
-            var number = Console.ReadLine();
-            var nodes = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c));
-
-            var res = MaxTreeDepth(nodes);
-            Console.WriteLine(res +1);
+            //generate the array with the size of the heap at index 0
+            var number = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c));
+            var arr = number.Concat(Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt64(c))).ToArray();
 
 
+            //while (true)
+            //{
+            //    var random = new Random();
+            //    var ArrLength = random.Next(0, 1000000);
+            //    var newArray = new long[ArrLength];
+            //    var notSorted = new long[ArrLength];
+            //    for (var i = 0; i < ArrLength; i++)
+            //    {
+            //        newArray[i] = random.Next(0, 1000000);
+            //        notSorted[i] = newArray[i];
+            //    }
+            //    MinHeapSort(newArray);
+
+            //    CheckHeap(newArray, 1);
+            //}
+
+
+            var res = MinHeapSort(arr);
+            //Console.WriteLine(res);
         }
     }
 }
