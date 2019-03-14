8a2037efc59bf654e00c795f19f9ba27c186e6ba
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2019-03-14 14:25:39 +1300

    completed contemporaneousTraversal

--- Program.cs
+++ Program.cs
@@ -11,166 +11,196 @@ namespace Addition
 
     class Program
     {
-        public class Contact
+        public class Node
         {
-            public long PhoneNumber { get; set; }
-            public string Name { get; set; }
+            public int Key { get; set; }
+            public Node LChild { get; set; }
+            public Node RChild { get; set; }
         }
 
-        public class HashMap
+        public static void InOrderTraversal(Node rootNode)
         {
-            private List<string>[] hashMap ; 
-            private long a = -1;
-            private long b = -1;
-            private int prime = 1000000007;
-            private ulong[] hashMemo = new ulong[50];
-
-            public HashMap(int m)
+            if(rootNode.LChild != null)
             {
-                hashMap= new List<string>[m];
+                InOrderTraversal(rootNode.LChild);
             }
-
-            private long HashLong(long numberToHash)
+            Console.Write(rootNode.Key + " ");
+            if(rootNode.RChild != null)
             {
-                if(a == -1)
-                {
-                    var randomGen = new Random();
-                    a = randomGen.Next(1, this.prime - 1);
-                    b = randomGen.Next(0, prime - 1);
-                }
-                var Hashed = (((a * numberToHash + b) % prime) % 5000);
-                return Hashed;
-            }
-
-
-            private ulong HashString(string stringToHash)
-            {
-                ulong total = 0;
-
-                for (int i = stringToHash.Length - 1; i >=0; i--)
-                {
-                    total = (total * 263 + Convert.ToUInt64(stringToHash[i])) % (ulong)prime;
-                }
-                return total % (ulong)hashMap.Length;
-
-            }
-
-            public string Find(string stringToFind)
-            {
-                var Hashed = HashString(stringToFind);
-                if(hashMap[Hashed] == null)
-                {
-                    return "no";
-                }
-                else if(hashMap[Hashed].Find(x=>x == stringToFind) != null)
-                {
-                    return "yes";
-                }
-                else
-                {
-                    return "no";
-                }
-            }
-
-            public void Add(string stringToAdd)
-            {
-                var Hashed = HashString(stringToAdd);
-                if(hashMap[Hashed] == null)
-                {
-                    hashMap[Hashed] = new List<string>();
-                }
-                var contact = hashMap[Hashed].SingleOrDefault(x => x == stringToAdd);
-                if (contact == null)
-                {
-                    hashMap[Hashed].Add(stringToAdd);
-                }
-
-            }
-
-            public void Delete(string stringToDelete)
-            {
-                var Hashed = HashString(stringToDelete);
-                if(hashMap[Hashed] != null)
-                {
-                    var contact = hashMap[Hashed].SingleOrDefault(x => x == stringToDelete);
-                    if (contact != null)
-                    {
-                        hashMap[Hashed].Remove(stringToDelete);
-                    }
-                }
-            }
-
-            public List<string> Check(long i)
-            {
-                if (hashMap[i] != null)
-                {
-                    return hashMap[i];
-                }
-                else return new List<string>();
-            }
-        }
-
-
-        public static void HashQueries(string[] queries, int m)
-        {
-            var HashSetInstance = new HashMap(m);
-            foreach(var query in queries)
-            {
-                var querieSegments = query.Split(' ');
-                var qs = querieSegments.Skip(1);
-                var sqr = string.Join(" ", qs);
-
-                switch (querieSegments[0])
-                {
-                    case "add":
-                        HashSetInstance.Add(sqr);
-                        break;
-                    case "del":
-                        HashSetInstance.Delete(sqr);
-                        break;
-                    case "find":
-                       var contact = HashSetInstance.Find(sqr);
-                        Console.WriteLine(contact);
-                        break;
-                    case "check":
-                        var HashBucket = HashSetInstance.Check(Convert.ToInt64(querieSegments[1]));
-                        //HashBucket.Reverse();
-                        if (HashBucket.Count == 0)
-                        {
-                            Console.WriteLine();
-                        }
-                        else
-                        {
-                           for(var i = HashBucket.Count -1; i>=0; i--)
-                            {
-                                Console.Write(HashBucket[i] + " ");
-                            }
-                            Console.WriteLine();
-                        }
-                        break;
-                }
+                InOrderTraversal(rootNode.RChild);
             }
             
-
         }
+
+        public static void PreOrderTraversal(Node rootNode)
+        {
+            Console.Write(rootNode.Key + " ");
+            if (rootNode.LChild != null)
+            {
+                PreOrderTraversal(rootNode.LChild);
+            }
+            
+            if (rootNode.RChild != null)
+            {
+                PreOrderTraversal(rootNode.RChild);
+            }
+        }
+
+        public static void PostOrderTraversal(Node rootNode)
+        {
+            
+            if (rootNode.LChild != null)
+            {
+                PostOrderTraversal(rootNode.LChild);
+            }
+
+            if (rootNode.RChild != null)
+            {
+                PostOrderTraversal(rootNode.RChild);
+            }
+            Console.Write(rootNode.Key + " ");
+        }
+
+        
+        public static void ContemporaneousTraversal(int[][] Tree)
+        {
+            var inOrderArray = new List<int>();
+            var preOrderArray = new List<int>();
+            var postOrderArray = new List<int>();
+            var traversalStack = new Stack<int>();
+
+            var CurrentIndex = 0;
+            traversalStack.Push(CurrentIndex);
+
+            while (traversalStack.Any())
+            {
+                CurrentIndex = traversalStack.Peek();
+
+                //if chech right child and add this to stack be processed last if it exists
+                if (Tree[CurrentIndex][2] != -1)
+                {
+                    traversalStack.Push(Tree[CurrentIndex][2]);
+                }
+
+                if (Tree[CurrentIndex][1] != -1)
+                {
+                    preOrderArray.Add(Tree[CurrentIndex][1]);
+                    traversalStack.Push(Tree[CurrentIndex][1]);
+                }
+
+                //if tree has no left but a right branch add it to inOrderTrav
+                if (Tree[CurrentIndex][1] == -1 && Tree[CurrentIndex][2] != -1)
+                {
+                    inOrderArray.Add(CurrentIndex);
+                }
+
+                if (Tree[CurrentIndex][1] == -1 && Tree[CurrentIndex][2] == -1)
+                {
+                    postOrderArray.Add(Tree[CurrentIndex][0]);
+                    traversalStack.Pop();
+                    if(!traversalStack.Any())
+                    CurrentIndex = traversalStack.Pop();
+
+                    //if no left children add the nodes key to inOrderTraversal Array
+                    if (Tree[CurrentIndex][2] != -1)
+                    {
+                        inOrderArray.Add(Tree[CurrentIndex][0]);
+                        traversalStack.Push(Tree[CurrentIndex][2]);
+
+                    }
+
+                }
+  
+
+
+
+                //then put th
+
+            }
+
+            //Array.ConvertAll(inOrderArray.ToArray(), x => Convert.ToString(x)).Join(' ');
+
+            //Console.WriteLine(string.Join(inOrderArray, " ");
+
+            foreach(var node in preOrderArray)
+            {
+                Console.Write(node + " ");
+            }
+            Console.WriteLine();
+        }
+
+    
+
+        public static void SingleTraversal(int[][]Tree, int Node, List<int>[] OrderArray)
+        {
+            OrderArray[1].Add(Tree[Node][0]);
+            if (Tree[Node][1] != -1)
+            {
+                SingleTraversal(Tree, Tree[Node][1], OrderArray);
+            }
+            OrderArray[0].Add(Tree[Node][0]);
+            if (Tree[Node][2] != -1)
+            {
+                SingleTraversal(Tree, Tree[Node][2], OrderArray);
+
+            }
+            OrderArray[2].Add(Tree[Node][0]);
+        }
+
+
+
 
 
         static void Main(string[] args)
         {
-
-            var m = Convert.ToInt32(Console.ReadLine());
             var numberOfQueries = Convert.ToInt32(Console.ReadLine());
-            var list = new String[numberOfQueries];
-            for (var i =0; i <numberOfQueries; i++)
+            //var nodeArray = new Node[numberOfQueries];
+
+            //for (var i = 0; i < numberOfQueries; i++)
+            //{
+            //    nodeArray[i] = new Node();
+            //}
+
+
+            var node2Array = new int[numberOfQueries][];
+
+            for (var i = 0; i < numberOfQueries; i++)
             {
-                list[i] = Console.ReadLine();
+                var input = Array.ConvertAll(Console.ReadLine().Split(' '), c => Convert.ToInt32(c));
+                //nodeArray[i].Key = input[0];
+                node2Array[i] = new int[3];
+                node2Array[i][0] = input[0];
+                node2Array[i][1] = input[1];
+                node2Array[i][2] = input[2];
+                //if (input[1] != -1)
+                //{
+                //    nodeArray[i].LChild = nodeArray[input[1]];
+                //}
+                //if (input[2] != -1)
+                //{
+                //    nodeArray[i].RChild = nodeArray[input[2]];
+                //}
             }
 
+            var OrderArrray = new List<int>[3];
 
+            for (var i = 0; i < 3; i++)
+            {
+                OrderArrray[i] = new List<int>();
+            }
 
+            //InOrderTraversal(nodeArray[0]);
+            //Console.WriteLine();
+            //PreOrderTraversal(nodeArray[0]);
+            //Console.WriteLine();
+            //PostOrderTraversal(nodeArray[0]);
 
-
-            HashQueries(list,m);
+            SingleTraversal(node2Array, 0, OrderArrray);
+            foreach(var list in OrderArrray)
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+            //ContemporaneousTraversal(node2Array);
         }
     }
 }
