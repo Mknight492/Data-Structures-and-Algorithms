9cac49b896f4fb19c01df32ea7f68bfcea890df2
Mknight492 <39689669+Mknight492@users.noreply.github.com>
2019-03-25 18:13:57 +1300

    completed burrow wheeler transformation sorting algorithm

--- Strings/BWTSortFail.cs
+++ Strings/BWTSortFail.cs
@@ -9,9 +9,9 @@ namespace Addition
 {
 
 
-    class Program
+    class BWTSortClass
     {
-      
+
 
         public static List<int> BWTMatching(char[] Word, char[][] Patterns)
         {
@@ -21,7 +21,7 @@ namespace Addition
             var GcountArray = new int[Word.Length];
             var TcountArray = new int[Word.Length];
 
-            for(var i=0;i< Word.Length; i++)
+            for (var i = 0; i < Word.Length; i++)
             {
                 AcountArray[i] = -1;
                 CcountArray[i] = -1;
@@ -35,7 +35,7 @@ namespace Addition
             var TCount = 0;
 
             //perform counting sort
-            for (var i= 0; i <Word.Length; i++)
+            for (var i = 0; i < Word.Length; i++)
             {
                 if (i > 0)
                 {
@@ -74,11 +74,11 @@ namespace Addition
 
             var numberOfMatchesPerPattern = new List<int>();
 
-            foreach(var Pattern in Patterns)
+            foreach (var Pattern in Patterns)
             {
                 var currentLetter = Pattern[0];
                 var currentMin = 0;
-                int currentMax = Word.Length -1;
+                int currentMax = Word.Length - 1;
 
                 var NoMatches = false;
                 if (Pattern.Length == 1)
@@ -103,12 +103,12 @@ namespace Addition
                         if (nextLetter == 'A')
                         {
                             var FirstOccurance = -1;
-                            for(var j= currentMin; (j<= currentMax && FirstOccurance == -1); j++)
+                            for (var j = currentMin; (j <= currentMax && FirstOccurance == -1); j++)
                             {
                                 if (j == 0 && AcountArray[0] == 0) FirstOccurance = j;
                                 else if (j > 0 && AcountArray[j] > AcountArray[j - 1]) FirstOccurance = j;
                             }
-                            if(FirstOccurance != -1)
+                            if (FirstOccurance != -1)
                             {
                                 var nextMin = AcountArray[FirstOccurance];
                                 if (nextMin == -1) nextMin = 0;
@@ -145,13 +145,13 @@ namespace Addition
                         else if (nextLetter == 'G')
                         {
                             var FirstOccurance = -1;
-                            for (var j = currentMin; (j <= currentMax && FirstOccurance == -1 ); j++)
+                            for (var j = currentMin; (j <= currentMax && FirstOccurance == -1); j++)
                             {
-                                if (j == 0 && GcountArray[0] == 0) FirstOccurance =j;
+                                if (j == 0 && GcountArray[0] == 0) FirstOccurance = j;
                                 else if (j > 0 && GcountArray[j] > GcountArray[j - 1]) FirstOccurance = j;
                             }
                             if (FirstOccurance != -1)
-                            { 
+                            {
                                 var nextMin = GcountArray[FirstOccurance];
                                 if (nextMin == -1) nextMin = 0;
                                 currentMin = firstG + nextMin;
@@ -173,7 +173,7 @@ namespace Addition
                                 else if (j > 0 && TcountArray[j] > TcountArray[j - 1]) FirstOccurance = j;
                             }
                             if (FirstOccurance != -1)
-                            { 
+                            {
                                 var nextMin = TcountArray[FirstOccurance];
                                 if (nextMin == -1) nextMin = 0;
                                 currentMin = firstT + nextMin;
@@ -196,7 +196,7 @@ namespace Addition
                         }
                     }
                 }
-                
+
             }
 
             return numberOfMatchesPerPattern;
@@ -205,93 +205,93 @@ namespace Addition
         }
 
 
-        static void Main(string[] args)
-        {
-            var InputString = Console.ReadLine().ToCharArray();
+        //static void Main(string[] args)
+        //{
+        //    var InputString = Console.ReadLine().ToCharArray();
 
-            var numberOfPatterns = Convert.ToInt32(Console.ReadLine());
+        //    var numberOfPatterns = Convert.ToInt32(Console.ReadLine());
 
-            var ListOfPatterns = new List<char[]>();
+        //    var ListOfPatterns = new List<char[]>();
 
-            var ArrayOfAllPatterns = Array.ConvertAll(Console.ReadLine().Split(' '), x => x.ToCharArray());
-
-       
-
-            var res =BWTMatching(InputString, ArrayOfAllPatterns);
-
-            Console.WriteLine(string.Join(" ", res));
-
-            //var cycles = 0;
-            //while (true)
-            //{
-            //    var randInst = new Random();
-            //    var numberOfLetters = randInst.Next(1, 500);
-            //    var letterArray = new char[numberOfLetters];
-
-            //    var patternArray = new char[randInst.Next(1, numberOfLetters)];
-
-            //    for (var i = 0; i < letterArray.Length; i++)
-            //    {
-            //        var nextLetter = randInst.Next(0, 3);
-            //        if (nextLetter == 0)
-            //            letterArray[i] = 'A';
-            //        if (nextLetter == 1)
-            //            letterArray[i] = 'C';
-            //        if (nextLetter == 2)
-            //            letterArray[i] = 'G';
-            //        if (nextLetter == 3)
-            //            letterArray[i] = 'T';
-            //    }
-
-            //    for (var i = 0; i < patternArray.Length; i++)
-            //    {
-            //        var nextLetter = randInst.Next(0, 3);
-            //        if (nextLetter == 0)
-            //            patternArray[i] = 'A';
-            //        if (nextLetter == 1)
-            //            patternArray[i] = 'C';
-            //        if (nextLetter == 2)
-            //            patternArray[i] = 'G';
-            //        if (nextLetter == 3)
-            //            patternArray[i] = 'T';
-            //    }
-            //    var PatternArrayList = new char[1][];
-            //    PatternArrayList[0] = patternArray;
-
-            //    var BWTstring = BurrowsWheelerTransformation.BWT((new string(letterArray) + "$").ToCharArray()).ToCharArray();
-
-            //    var matchesViaBWTImplimentation = BWTMatching(BWTstring, PatternArrayList)[0];
-
-            //    var matchesViaNaiveAlg = NaiveMatching(letterArray, PatternArrayList[0]);
+        //    var ArrayOfAllPatterns = Array.ConvertAll(Console.ReadLine().Split(' '), x => x.ToCharArray());
 
 
-            //    if (matchesViaBWTImplimentation != matchesViaNaiveAlg)
-            //    {
 
-            //        var incorrectMatches = BWTMatching(BWTstring, PatternArrayList);
-            //        cycles = 0;
-            //    }
-            //    cycles++;
+        //    var res = BWTMatching(InputString, ArrayOfAllPatterns);
 
-            //}
-        }
+        //    Console.WriteLine(string.Join(" ", res));
+
+        //    //var cycles = 0;
+        //    //while (true)
+        //    //{
+        //    //    var randInst = new Random();
+        //    //    var numberOfLetters = randInst.Next(1, 500);
+        //    //    var letterArray = new char[numberOfLetters];
+
+        //    //    var patternArray = new char[randInst.Next(1, numberOfLetters)];
+
+        //    //    for (var i = 0; i < letterArray.Length; i++)
+        //    //    {
+        //    //        var nextLetter = randInst.Next(0, 3);
+        //    //        if (nextLetter == 0)
+        //    //            letterArray[i] = 'A';
+        //    //        if (nextLetter == 1)
+        //    //            letterArray[i] = 'C';
+        //    //        if (nextLetter == 2)
+        //    //            letterArray[i] = 'G';
+        //    //        if (nextLetter == 3)
+        //    //            letterArray[i] = 'T';
+        //    //    }
+
+        //    //    for (var i = 0; i < patternArray.Length; i++)
+        //    //    {
+        //    //        var nextLetter = randInst.Next(0, 3);
+        //    //        if (nextLetter == 0)
+        //    //            patternArray[i] = 'A';
+        //    //        if (nextLetter == 1)
+        //    //            patternArray[i] = 'C';
+        //    //        if (nextLetter == 2)
+        //    //            patternArray[i] = 'G';
+        //    //        if (nextLetter == 3)
+        //    //            patternArray[i] = 'T';
+        //    //    }
+        //    //    var PatternArrayList = new char[1][];
+        //    //    PatternArrayList[0] = patternArray;
+
+        //    //    var BWTstring = BurrowsWheelerTransformation.BWT((new string(letterArray) + "$").ToCharArray()).ToCharArray();
+
+        //    //    var matchesViaBWTImplimentation = BWTMatching(BWTstring, PatternArrayList)[0];
+
+        //    //    var matchesViaNaiveAlg = NaiveMatching(letterArray, PatternArrayList[0]);
+
+
+        //    //    if (matchesViaBWTImplimentation != matchesViaNaiveAlg)
+        //    //    {
+
+        //    //        var incorrectMatches = BWTMatching(BWTstring, PatternArrayList);
+        //    //        cycles = 0;
+        //    //    }
+        //    //    cycles++;
+
+        //    //}
+        //}
 
         public static int NaiveMatching(char[] letterArrray, char[] Pattern)
         {
             var numberOfMatches = 0;
 
-            for (var i =0; i<=letterArrray.Length - Pattern.Length; i++)
+            for (var i = 0; i <= letterArrray.Length - Pattern.Length; i++)
             {
                 if (letterArrray[i] == Pattern[0])
                 {
                     var match = true;
                     var count = 0;
-                    for (var j = i; (j<letterArrray.Length &&  count < Pattern.Length); j++)
+                    for (var j = i; (j < letterArrray.Length && count < Pattern.Length); j++)
                     {
-                       
+
                         if (letterArrray[j] != Pattern[count])
                         {
-                            
+
                             match = false;
                         }
                         count++;
@@ -304,7 +304,7 @@ namespace Addition
 
             return numberOfMatches;
         }
-        
+
     }
 }
 
